9eff3cd67855eab766af382d095af6cff27f29a7
Damiansjj <ellis.viracocha.getial@student.ehb.be>
2024-09-27 15:45:09 +0200

    Create oef8.mjs

---
+++ oef8.mjs
@@ -0,0 +1,10 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+let jaartal = parseFloat (await userInput.question ('Geef een jaartal? '))
+
+if((jaartal %4== 0 && jaartal %100!==0) ||(jaartal %400==0))
+
+    {console.log("schrikkeljaar, februari heeft 29 dagen")}
+else{console.log("geen schrikkeljaar, februari heeft 28 dagen")}
